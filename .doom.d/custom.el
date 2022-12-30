ed6e49e975a297905458c7fe4ce125242080003d
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2022-12-30 15:55:02 -0600

    Doom-Emacs

---
+++ .doom.d/custom.el
@@ -0,0 +1,25 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("180641b59315dcbf610ac82acbd80eb878ed969ba57a371345b4c58cff35d309" "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" default))
+ '(package-selected-packages '(eglot))
+ '(reftex-section-levels
+   '(("part" . 0)
+     ("chapter" . 1)
+     ("section" . 2)
+     ("subsection" . 3)
+     ("subsubsection" . 4)
+     ("paragraph" . 5)
+     ("subparagraph" . 6)
+     ("addchap" . -1)
+     ("addsec" . -2)
+     ("frame" . -3))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
