76ddc7bce3b4917d55af68138a927d07503bac2c
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2023-01-02 21:05:26 -0600

    Latex Config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -381,3 +381,10 @@
 (setq ein:output-area-inlined-images t)
 
 (add-hook'pdf-tools-enabled-hook 'pdf-view-themed-minor-mode)
+;; Use pdf-tools to open PDF files
+(setq TeX-view-program-selection '((output-pdf "PDF Tools"))
+      TeX-source-correlate-start-server t)
+
+;; Update PDF buffers after successful LaTeX runs
+(add-hook 'TeX-after-compilation-finished-functions
+           #'TeX-revert-document-buffer)
