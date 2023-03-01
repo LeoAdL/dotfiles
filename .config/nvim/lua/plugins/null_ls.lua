ebe316f61a0c2c942f4e43ea6e3f9d268d6c3c28
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2023-02-28 23:33:26 -0600

    Add nvim config

---
+++ .config/nvim/lua/plugins/null_ls.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function()
+      local nls = require("null-ls")
+      return {
+        sources = {
+          nls.builtins.formatting.black,
+          nls.builtins.formatting.latexindent,
+          nls.builtins.diagnostics.ruff,
+          nls.builtins.formatting.isort,
+          nls.builtins.code_actions.proselint,
+          nls.builtins.formatting.ruff,
+          nls.builtins.diagnostics.chktex,
+          nls.builtins.completion.luasnip,
+          nls.builtins.diagnostics.luacheck,
+          nls.builtins.formatting.stylua,
+          nls.builtins.code_actions.gitsigns,
+          nls.builtins.formatting.prettierd,
+          nls.builtins.formatting.stylua,
+        },
+      }
+    end,
+  },
+}
