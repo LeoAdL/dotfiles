76d843980bf18fb2ccfd50d24108d60cc21f8bbd
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2023-03-02 00:07:33 -0600

    Neogit: add keybindings

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -4,5 +4,17 @@ return {
     config = function()
       require("neogit").setup()
     end,
+    keys = {
+      {
+        "<leader>gg",
+        function()
+          local neogit = require("neogit")
+
+          -- open using defaults
+          neogit.open()
+        end,
+        "Neogit",
+      },
+    },
   },
 }
