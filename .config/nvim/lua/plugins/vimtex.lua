71b0e3dea0152f8a48cc66240d1e305ff62b23f1
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2023-03-05 22:27:15 -0600

    Fix latexmk

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -5,6 +5,9 @@ return {
       vim.g.vimtex_view_method = "sioyek"
       vim.g.maplocalleader = " "
       vim.g.conceallevel = 2
+      vim.g.vimtex_compiler_latexmk = {
+        options = { "-pdf", "-shell-escape", "-verbose", "-file-line-error", "-synctex=1", "-interaction=nonstopmode" },
+      }
     end,
   },
 }
