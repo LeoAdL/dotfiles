2ad02deaefb270d3ff9c08b9574763f0d1270af1
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2023-03-08 00:06:24 -0600

    Catpuccin *everything*

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -14,7 +14,7 @@ return {
         show_end_of_buffer = false, -- show the '~' characters after the end of buffers
         term_colors = true,
         dim_inactive = {
-          enabled = true,
+          enabled = false,
           shade = "dark",
           percentage = 0.15,
         },
