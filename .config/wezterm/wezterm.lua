ecb59f4b8ebcd55e519e89947c4a2e12ddd1469d
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2023-04-03 23:38:43 -0500

    SF Mono switch

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,9 +2,9 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 return {
 	font = wezterm.font({
-		family = "Iosevka Nerd Font",
+		family = "SF Mono Nerd Font",
 	}),
-	font_size = 16.0,
+	font_size = 15.0,
 	color_scheme = "nord",
 	tab_bar_at_bottom = true,
 	hide_tab_bar_if_only_one_tab = true,
