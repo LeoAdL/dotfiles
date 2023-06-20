81c6954d734facd2e015be7ea8b489efde7dab00
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2023-06-19 19:36:57 -0500

    update nvim plugins

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -3,22 +3,23 @@ return {
         "hrsh7th/nvim-cmp",
         ---@param opts cmp.ConfigSchema
         opts = function(_, opts)
-            local has_words_before = function()
-                unpack = unpack or table.unpack
-                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-                return col ~= 0
-                    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-            end
-
             local luasnip = require("luasnip")
             local cmp = require("cmp")
             opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "neorg" } }))
             opts.sources = cmp.config.sources({
                 { name = "nvim_lsp" },
-                { name = "luasnip" },
+                { name = "luasnip", option = { show_autosnippets = true } },
                 { name = "buffer" },
                 { name = "path" },
             })
+            opts.snippet = {
+                -- REQUIRED - you must specify a snippet engine
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+                    -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+                    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+                end,
+            }
             opts.mapping = vim.tbl_extend("force", opts.mapping, {
                 ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
--- .config/nvim/lua/plugins/fidget.lua
+++ .config/nvim/lua/plugins/fidget.lua
@@ -10,6 +10,6 @@ return {
                 },
             })
         end,
-        enabled = false,
+        tag = "legacy",
     },
 }
