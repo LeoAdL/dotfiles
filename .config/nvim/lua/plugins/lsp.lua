c5b84b642a89f8e7554f6f69263e10d2b90f903a
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2023-06-28 10:43:16 -0500

    Fix Ltex-ls code actions

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -95,4 +95,15 @@ return {
             },
         },
     },
+    {
+        "barreiroleo/ltex_extra.nvim",
+        ft = { "markdown", "tex" },
+        dependencies = { "neovim/nvim-lspconfig" },
+        -- yes, you can use the opts field, just I'm showing the setup explicitly
+        config = function()
+            require("ltex_extra").setup({
+                server_opts = {},
+            })
+        end,
+    },
 }
