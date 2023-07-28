1dd04c5ef4936be4ca59f42ceb0abf37c2291903
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2023-07-28 22:39:49 +0200

    Slight Vim Update

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -9,8 +9,8 @@ return {
       vim.g.vimtex_quickfix_open_on_warning = 0
       vim.g.vimtex_view_skim_sync = 1
       vim.g.vimtex_view_skim_reading_bar = 1
-      vim.g.vimtex_syntax_enabled = 1
-      vim.g.vimtex_syntax_conceal_disable = 0
+      vim.g.vimtex_syntax_enabled = 0
+      vim.g.vimtex_syntax_conceal_disable = 1
       vim.g.conceallevel = 2
       vim.g.vimtex_quickfix_enabled = 1
       vim.g.vimtex_compiler_latexmk = {
