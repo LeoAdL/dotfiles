cab6a689538e00b7d3e9a9ed7e33f4d35241cfd1
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2023-11-10 17:31:53 -0600

    Neovim: Final config

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -1,3 +1,5 @@
 return {
-    { "NvChad/nvim-colorizer.lua", config = true, opts = { user_default_options = { names = false } } },
+    { "NvChad/nvim-colorizer.lua",           config = true, opts = { user_default_options = { names = false } } },
+    { "nvim-tree/nvim-web-devicons" },
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl",  opts = {} },
 }
