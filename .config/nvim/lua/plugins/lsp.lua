9b3f231006fde827c9a98f6d53bef3685b0a820b
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2024-04-11 14:34:07 -0500

    Fix Ltex-extra

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -14,19 +14,7 @@ return {
             }
         end
     },
-    {
-        "jhofscheier/ltex-utils.nvim",
-        dependencies = {
-            "neovim/nvim-lspconfig",
-            "nvim-telescope/telescope.nvim",
-            -- "nvim-telescope/telescope-fzf-native.nvim", -- optional
-        },
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-    },
+    { "barreiroleo/ltex_extra.nvim" },
     {
         "neovim/nvim-lspconfig",
         opts = {
@@ -94,7 +82,21 @@ return {
 
                     on_attach = function(client, bufnr)
                         -- rest of your on_attach process.
-                        require("ltex-utils").on_attach(bufnr)
+                        require("ltex_extra").setup {
+                            -- table <string> : languages for witch dictionaries will be loaded, e.g. { "es-AR", "en-US" }
+                            -- https://valentjn.github.io/ltex/supported-languages.html#natural-languages
+                            load_langs = { "en-US" }, -- en-US as default
+                            -- boolean : whether to load dictionaries on startup
+                            init_check = true,        -- string : relative or absolute path to store dictionaries
+                            -- e.g. subfolder in the project root or the current working directory: ".ltex"
+                            -- e.g. shared files for all projects:  vim.fn.expand("~") .. "/.local/share/ltex"
+                            path = "", -- project root or current working directory
+                            -- string : "none", "trace", "debug", "info", "warn", "error", "fatal"
+                            log_level = "none",
+                            -- table : configurations of the ltex language server.
+                            -- Only if you are calling the server from ltex_extra
+                            server_opts = nil
+                        }
                     end,
                     settings = {
                         ltex = {
