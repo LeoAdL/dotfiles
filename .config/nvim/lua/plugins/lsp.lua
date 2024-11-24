7f662b45b558738e1aad19cf24e2ffe2e2a25998
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2024-11-23 19:29:15 -0500

    Clean nvim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -5,7 +5,7 @@ return {
     },
     {
         "WhoIsSethDaniel/mason-tool-installer.nvim",
-        opts = { ensure_installed = { "beautysh", "latexindent", "yamlfmt", "markdownlint", "markdownlint-cli2", "selene" } },
+        opts = { ensure_installed = { "selene" } },
     },
     {
         "williamboman/mason-lspconfig.nvim",
@@ -19,7 +19,7 @@ return {
         opts = {
             -- options for vim.diagnostic.config()
             diagnostics = {
-                underline = true,
+                underline = false,
                 update_in_insert = false,
                 virtual_text = false,
                 severity_sort = true,
@@ -67,7 +67,6 @@ return {
                     },
                 },
                 marksman = {},
-                julials = {},
                 basedpyright = {
                 },
                 ltex = {
@@ -118,9 +117,9 @@ return {
         config = function(_, opts)
             local mason_lspconfig = require 'mason-lspconfig'
             local servers = opts.servers
-            mason_lspconfig.setup {
-                ensure_installed = vim.tbl_keys(servers),
-            }
+            -- mason_lspconfig.setup {
+            --     ensure_installed = vim.tbl_keys(servers),
+            -- }
             local capabilities = vim.lsp.protocol.make_client_capabilities()
             capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
             local handlers = { function(server_name)
