c8a3d81a1f7967d8955fd71822a7bc34825d9ed3
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2025-01-09 17:20:04 -0500

    Fix Path for emacs

--- .emacs.d/post-init.el
+++ .emacs.d/post-init.el
@@ -40,6 +40,13 @@
 ;; the precise point where you previously left off.
 (add-hook 'elpaca-after-init-hook #'save-place-mode)
 
+(use-package exec-path-from-shell
+  :ensure t
+  :demand t
+  :config
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize))
+  )
 (use-package doom-themes
   :ensure t
   :demand t
