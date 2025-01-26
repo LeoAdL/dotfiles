d3ef4b5f8d49b769e553a6c0c241fa726f5e0c15
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2025-01-26 17:25:01 -0500

    Switch do mini.icons

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -10,6 +10,6 @@ return {
             }
         },
         -- Optional dependencies
-        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        dependencies = { { "echasnovski/mini.icons", opts = {} } },
     }
 }
