06787b349d4040179c46f73c9efd1201ef234328
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2025-01-26 15:26:32 -0500

    Timemachine

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,6 +6,7 @@ vim.keymap.set("n", "<leader>.", "<cmd>Oil<cr>", { desc = "file_browser" })
 vim.keymap.set("n", "<leader>lL", "<cmd>Lazy<cr>", { desc = "Lazy" })
 vim.keymap.set("n", "<leader>xg", "<cmd>Gitsigns setqflist<cr>", { desc = "Git QuickFix" })
 vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<cr>", { desc = "NeoGit" })
+vim.keymap.set("n", "<leader>gt", "<cmd>Tardis<cr>", { desc = "Timemachine" })
 local bufopts = { noremap = true, silent = true, desc = "Expand LSP diagnostic" }
 
 vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, bufopts)
---
+++ .config/nvim/lua/plugins/tardis.lua
@@ -0,0 +1,6 @@
+return {
+    'fredeeb/tardis.nvim',
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {}
+}
