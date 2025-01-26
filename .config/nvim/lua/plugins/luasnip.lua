761448dcdab3ba3e967114d76f2135b8f56fd304
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2025-01-26 14:06:04 -0500

    Make luasnip lazy

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -1,6 +1,7 @@
 return {
     {
         "L3MON4D3/LuaSnip",
+        lazy = true,
         build = "make install_jsregexp",
         config = function()
             require("luasnip.loaders.from_vscode").lazy_load({ paths = { "/Users/leoap/.config/nvim/lua/snippets" } })
