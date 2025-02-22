b4bc12cc339546c03c142c8dfa1c2bf7f4c02d1f
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2025-02-22 11:47:38 +0100

    Update elpaca

--- .emacs.d/pre-init.el
+++ .emacs.d/pre-init.el
@@ -5,7 +5,7 @@
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-                              :ref nil :depth 1
+                              :ref nil :depth 1 :inherit ignore
                               :files (:defaults "elpaca-test.el" (:exclude "extensions"))
                               :build (:not elpaca--activate-package)))
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
@@ -42,9 +42,3 @@
 ;; Optional: Install use-package support
 (elpaca elpaca-use-package
   (elpaca-use-package-mode))
-
-(elpaca (queue
-         :host github
-         :repo "emacsmirror/queue"
-         :inherit nil))
-
