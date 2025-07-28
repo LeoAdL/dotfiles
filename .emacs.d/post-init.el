2f7f71a880c5546ab06a488d59b58fd48d198506
Leo Aparisi de Lannoy <leoaparisi@gmail.com>
2025-07-28 19:26:29 -0400

    Update Auto-revert

--- .emacs.d/post-init.el
+++ .emacs.d/post-init.el
@@ -77,26 +77,65 @@
 ;; Auto-revert in Emacs is a feature that automatically updates the
 ;; contents of a buffer to reflect changes made to the underlying file
 ;; on disk.
-(add-hook 'elpaca-after-init-hook #'global-auto-revert-mode)
+(use-package autorevert
+  :ensure nil
+  :commands (auto-revert-mode global-auto-revert-mode)
+  :hook
+  (elpaca-after-init . global-auto-revert-mode))
+
 
 ;; recentf is an Emacs package that maintains a list of recently
 ;; accessed files, making it easier to reopen files you have worked on
 ;; recently.
-(add-hook 'elpaca-after-init-hook #'(lambda()
-                                      (let ((inhibit-message t))
-                                        (recentf-mode 1))))
-(add-hook 'kill-emacs-hook #'recentf-cleanup)
+(use-package recentf
+  :ensure nil
+  :commands (recentf-mode recentf-cleanup)
+  :hook
+  (elpaca-after-init . recentf-mode)
+
+  :custom
+  (recentf-auto-cleanup (if (daemonp) 300 'never))
+  (recentf-exclude
+   (list "\\.tar$" "\\.tbz2$" "\\.tbz$" "\\.tgz$" "\\.bz2$"
+         "\\.bz$" "\\.gz$" "\\.gzip$" "\\.xz$" "\\.zip$"
+         "\\.7z$" "\\.rar$"
+         "COMMIT_EDITMSG\\'"
+         "\\.\\(?:gz\\|gif\\|svg\\|png\\|jpe?g\\|bmp\\|xpm\\)$"
+         "-autoloads\\.el$" "autoload\\.el$"))
+
+  :config
+  ;; A cleanup depth of -90 ensures that `recentf-cleanup' runs before
+  ;; `recentf-save-list', allowing stale entries to be removed before the list
+  ;; is saved by `recentf-save-list', which is automatically added to
+  ;; `kill-emacs-hook' by `recentf-mode'.
+  (add-hook 'kill-emacs-hook #'recentf-cleanup -90))
 
 ;; savehist is an Emacs feature that preserves the minibuffer history between
 ;; sessions. It saves the history of inputs in the minibuffer, such as commands,
 ;; search strings, and other prompts, to a file. This allows users to retain
 ;; their minibuffer history across Emacs restarts.
-(add-hook 'elpaca-after-init-hook #'savehist-mode)
+(use-package savehist
+  :ensure nil
+  :commands (savehist-mode savehist-save)
+  :hook
+  (after-init . savehist-mode)
+  :custom
+  (savehist-autosave-interval 600)
+  (savehist-additional-variables
+   '(kill-ring                        ; clipboard
+     register-alist                   ; macros
+     mark-ring global-mark-ring       ; marks
+     search-ring regexp-search-ring)))
 
 ;; save-place-mode enables Emacs to remember the last location within a file
 ;; upon reopening. This feature is particularly beneficial for resuming work at
 ;; the precise point where you previously left off.
-(add-hook 'elpaca-after-init-hook #'save-place-mode)
+;; the precise point where you previously left off.
+(use-package saveplace
+  :ensure nil
+  :commands (save-place-mode save-place-local-mode)
+  :hook
+  (elpaca-after-init . save-place-mode))
 
 ;; Idle garbage collection
 
@@ -1495,17 +1534,6 @@
 ;;   :init
 ;;   (add-hook 'emacs-startup-hook #'easysession-load-including-geometry 102)
 ;;   (add-hook 'emacs-startup-hook #'easysession-save-mode 103))
-
-(use-package savehist
-  :ensure nil
-  :config
-  (setopt savehist-save-minibuffer-history t
-          savehist-autosave-interval nil)
-  (add-to-list 'savehist-additional-variables 'kill-ring)
-  (add-to-list 'savehist-additional-variables 'mark-ring)
-  (add-to-list 'savehist-additional-variables 'search-ring)
-  ;; (add-to-list 'savehist-additional-variables 'easysession--current-session-name)
-  (add-to-list 'savehist-additional-variables 'regexp-search-ring))
 
 (use-package ws-butler
   :ensure t
