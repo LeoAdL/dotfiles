69b8bb86cd228fc09b490612db0bf064d5c5235d
Leo Aparisi de Lannoy <leoaparisi@gmail.com>
2026-06-19 11:05:58 -0400

    Consolidate

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -21,8 +21,9 @@ vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
 -- Diagnostics
 -- ==============================================================================
 
--- Show line diagnostics
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+-- Show line diagnostics and loclist
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic error messages" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic loclist" })
 
 -- Helper function for jumping between diagnostics
 local diagnostic_goto = function(next, severity)
@@ -39,44 +40,3 @@ vim.keymap.set("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" 
 vim.keymap.set("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
 vim.keymap.set("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
 vim.keymap.set("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
-
--- ==============================================================================
--- LSP Keymaps
--- ==============================================================================
-
--- Use LspAttach autocommand to only map the following keys
--- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('UserLspConfig', { clear = true }),
-    callback = function(ev)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-        -- Helper function to easily map LSP keys with descriptions
-        local map = function(keys, func, desc, mode)
-            mode = mode or 'n'
-            vim.keymap.set(mode, keys, func, { buffer = ev.buf, desc = "LSP: " .. desc })
-        end
-
-        map('gD', vim.lsp.buf.declaration, 'Goto declaration')
-        map('gd', vim.lsp.buf.definition, 'Goto definition')
-        map('gi', vim.lsp.buf.implementation, 'Goto implementation')
-        map('gr', vim.lsp.buf.references, 'Goto references')
-        map('K', vim.lsp.buf.hover, 'Hover Documentation')
-        map('<C-k>', vim.lsp.buf.signature_help, 'Signature help')
-
-        map('<space>D', vim.lsp.buf.type_definition, 'Type definition')
-        map('<space>rn', vim.lsp.buf.rename, 'Rename')
-        map('<space>ca', vim.lsp.buf.code_action, 'Code action', { 'n', 'v' })
-
-        map('<space>wa', vim.lsp.buf.add_workspace_folder, 'Add workspace folder')
-        map('<space>wr', vim.lsp.buf.remove_workspace_folder, 'Remove workspace folder')
-        map('<space>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, 'List workspace folders')
-
-        map('<space>fF', function()
-            vim.lsp.buf.format { async = true }
-        end, 'Format buffer')
-    end,
-})
